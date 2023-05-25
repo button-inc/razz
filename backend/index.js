6ca2f1039b5a7873979a892ff3e4e6b5541b04cf
tmastrom <thomas.mastromonaco@gmail.com>
2023-05-25 10:16:37 -0700

    auth handshake

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const cookieParser = require('cookie-parser')
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -12,44 +13,74 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  res.send({ express: 'express backend' });
 })
 
-app.get("/api/github/:code", (req, res) => {
+app.post("/auth/login", (req, res) => {
+  const location = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`
+  res.redirect(location);
+})
 
-  const { code } = req.params;
+app.get("/auth/exchange/", (req, res) => {
+  const code = req.query.code;
+  // env params to request access token
   const params = {
-      "client_id": process.env.CLIENT_ID,
-      "client_secret": process.env.CLIENT_SECRET,
-      "code": code
+    "client_id": process.env.CLIENT_ID,
+    "client_secret": process.env.CLIENT_SECRET,
+    "code": code
   }
 
+  // request access token
   axios.post(
-      "https://github.com/login/oauth/access_token",
-      params,
-      {
-          headers: {
-              "Content-Type": "application/json",
-          }
-      }
+    "https://github.com/login/oauth/access_token",
+    params,
+    {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
   )
   .then(response => {
-    const data = response.data.split('&')
+    const access_token = new URLSearchParams(response.data).get("access_token")
+    console.log(access_token)
 
-    if (data[0].includes("access_token")) {
-      const token = data[0].split('=')[1]
-      console.log('got token')
-      res.status(200).send({"token": token})
-    }
-    else if (data[0].includes("error")){
-      console.log('error')
-      res.status(400).send({"error": data[0]})
-    }
+    // put access_token in cookie session
+    res.cookie(`token`, `${access_token}`);
+    res.redirect('http://localhost:5173/dashboard')
   })
   .catch(error => {
-    res.send(JSON.stringify(error));
+    // TODO: handle errors
+    console.log('err')
   });
-});
+})
+
+app.get("/github/repo", (req, res) => {
+  console.log('/github/repo')
+  console.log(req.cookies)
+  res.send(req.cookies);
+})
+
+app.get("/github/issues", (req, res) => {
+  axios.get(`https://api.github.com/repos/${repo}/issues`, {
+    method: "GET",
+    headers: {
+        "Accept": "application/vnd.github+json",
+        "Authorization": `Bearer ${token.token}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+  })
+  const data = response.json()
+
+  const issues = data.map((item) => {
+      const container = {};
+
+      container.title = item.title;
+      container.number = item.number;
+      container.url = item.url;
+
+      return container;
+  })
+})
 
 app.listen(port, function () {
   console.log("Runnning on " + port);
--- frontend/src/routes/dashboard.jsx
+++ frontend/src/routes/dashboard.jsx
@@ -1,111 +1,21 @@
-import { useMemo, useState } from "react";
-import { Outlet, Link, useLoaderData } from "react-router-dom";
 import Navbar from "../components/navbar";
 
-// exchange GitHub redirect code for user access token
-export function loader ({ request }) {
-    const url = new URL(request.url);
-    const code = url.searchParams.get('code')
-    return fetch(`http://localhost:3001/api/github/${code}`);
-}
-
 export default function Dashboard() {
-    const [token, setToken] = useState(useLoaderData());
-    const [repos, setRepos] = useState([])
-    const [issues, setIssues] = useState([])
 
-    async function getUserRepos () {
-        // Lists repositories that the authenticated user has explicit permission (:read, :write, or :admin) to access.
-        const response = await fetch("https://api.github.com/user/repos?affiliation=collaborator&sort=updated", {
+    const getRepos = async () => {
+        const response = await fetch("http://localhost:3001/github/repo", {
             method: "GET",
-            headers: {
-                "Accept": "application/vnd.github+json",
-                "Authorization": `Bearer ${token.token}`,
-                "X-GitHub-Api-Version": "2022-11-28",
-            },
-        })
-
-        const data = await response.json();
-        const repos = data.map((item) => item.full_name) // item.full_name == OWNER/REPO
-        setRepos(repos)
-    }
-
-    if (token.token && !repos.length) {
-        getUserRepos()
-    }
-
-    // List issues in a repository. Only open issues will be listed.
-    // https://api.github.com/repos/OWNER/REPO/issues
-    async function getRepoIssues (repo) {
-        // Lists repositories that the authenticated user has explicit permission (:read, :write, or :admin) to access.
-        const response = await fetch(`https://api.github.com/repos/${repo}/issues`, {
-            method: "GET",
-            headers: {
-                "Accept": "application/vnd.github+json",
-                "Authorization": `Bearer ${token.token}`,
-                "X-GitHub-Api-Version": "2022-11-28",
-            }
         })
         const data = await response.json()
 
-        const issues = data.map((item) => {
-            const container = {};
-
-            container.title = item.title;
-            container.number = item.number;
-            container.url = item.url;
-
-            return container;
-        })
-        setIssues(issues)
+        console.log(data)
     }
-
-    function handleOnRepoClick (repo) {
-        getRepoIssues(repo)
-    }
-
-    // button to "import github issues"
-    // opens modal in which the user can navigate repos and select issues
-    // TODO: paginate requests if many repos/issues
-    // TODO: add search
 
     return (
       <>
         <Navbar />
-        {/* <div id="sidebar">
-            <h1>React Router Contacts</h1>
-            <div>
-            <form id="search-form" role="search">
-                <input
-                    id="q"
-                    aria-label="Search contacts"
-                    placeholder="Search"
-                    type="search"
-                    name="q"
-                />
-                <div
-                    id="search-spinner"
-                    aria-hidden
-                    hidden={true}
-                />
-                <div
-                    className="sr-only"
-                    aria-live="polite"
-                ></div>
-            </form>
-            <form method="post">
-                <button type="submit">New</button>
-            </form>
-            </div>
-        </div> */}
-        <div>
-            {repos.map((item) => <li key={item} onClick={() => handleOnRepoClick(item)}>{item}</li>)}
-        </div>
-        {issues.length &&
-            <div>
-                {issues.map((item) => <li key={item.number}>{item.title}</li>)}
-            </div>
-        }
+        <div> Dashboard </div>
+        <button onClick={() => getRepos()}>Github Repos</button>
       </>
     );
 }
