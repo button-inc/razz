9d07022887ec890af06bb7e1ebfd570e74934201
tmastrom <thomas.mastromonaco@gmail.com>
2023-05-25 13:14:37 -0700

    session cookie

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
-const cookieParser = require('cookie-parser')
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const axios = require("axios")
+const tough = require("tough-cookie");
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,9 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const Cookie = tough.Cookie;
+const cookiejar = new tough.CookieJar();
 
 app.get("/", (req, res) => {
   res.send({ express: 'express backend' });
@@ -21,7 +25,7 @@ app.post("/auth/login", (req, res) => {
   res.redirect(location);
 })
 
-app.get("/auth/exchange/", (req, res) => {
+app.get("/auth/exchange/", (req, res, next) => {
   const code = req.query.code;
   // env params to request access token
   const params = {
@@ -42,44 +46,82 @@ app.get("/auth/exchange/", (req, res) => {
   )
   .then(response => {
     const access_token = new URLSearchParams(response.data).get("access_token")
-    console.log(access_token)
+    const cookie = Cookie.parse(`token=${access_token}`)
 
-    // put access_token in cookie session
-    res.cookie(`token`, `${access_token}`);
-    res.redirect('http://localhost:5173/dashboard')
+    cookiejar.setCookie(
+      cookie,
+      "http://localhost:3001/",
+      function (err, cookie) {
+        if (err) {
+          console.log('err', err)
+          res.redirect('http://localhost:5173/')
+        }
+        res.redirect('http://localhost:5173/dashboard')
+      }
+    );
+
   })
   .catch(error => {
     // TODO: handle errors
-    console.log('err')
+    console.log('err', error)
+    next(error)
   });
 })
 
 app.get("/github/repo", (req, res) => {
   console.log('/github/repo')
-  console.log(req.cookies)
-  res.send(req.cookies);
+  // get the token cookie
+  cookiejar.getCookies("http://localhost:3001/", function (err, cookies) {
+    if (err) {
+      console.log('error retrieving cookie', err)
+      res.redirect('http://localhost:5173/')
+    }
+
+    const token = cookies[0].value;
+    // get repos from github using token
+    axios.get(
+      "https://api.github.com/user/repos?affiliation=collaborator&per_page=100",
+      {
+        headers: {
+          "Accept": "application/vnd.github+json",
+          "Authorization": `Bearer ${token}`,
+          "X-GitHub-Api-Version": "2022-11-28"
+        }
+      }
+    )
+    .then(response => {
+      const data = response.data;
+      const repos = data.map(repo => repo.full_name)
+      res.send(repos)
+    })
+    .catch(err => {
+      console.log('error fetching repos: ', err)
+      res.redirect('http://localhost:5173/')
+    })
+});
+
 })
 
 app.get("/github/issues", (req, res) => {
-  axios.get(`https://api.github.com/repos/${repo}/issues`, {
-    method: "GET",
-    headers: {
-        "Accept": "application/vnd.github+json",
-        "Authorization": `Bearer ${token.token}`,
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
-  })
-  const data = response.json()
+  // axios.get(`https://api.github.com/repos/${repo}/issues`, {
+  //   method: "GET",
+  //   headers: {
+  //       "Accept": "application/vnd.github+json",
+  //       "Authorization": `Bearer ${token.token}`,
+  //       "X-GitHub-Api-Version": "2022-11-28",
+  //   }
+  // })
+  // const data = response.json()
 
-  const issues = data.map((item) => {
-      const container = {};
+  // const issues = data.map((item) => {
+  //     const container = {};
 
-      container.title = item.title;
-      container.number = item.number;
-      container.url = item.url;
+  //     container.title = item.title;
+  //     container.number = item.number;
+  //     container.url = item.url;
 
-      return container;
-  })
+  //     return container;
+  // })
 })
 
 app.listen(port, function () {
