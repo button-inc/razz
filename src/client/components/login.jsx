8d9b729089430ab39b770ebe8cf3964c2c74edcf
tmastrom <thomas.mastromonaco@gmail.com>
2023-05-26 09:59:29 -0700

    use vite-express

--- src/client/components/login.jsx
+++ src/client/components/login.jsx
@@ -1,7 +1,9 @@
 export default function Login() {
+
+  const URL = import.meta.env.BASE_URL + 'auth/login';
   return (
     <>
-      <form action="http://localhost:3001/auth/login" method="POST">
+      <form action={URL} method="POST">
         <input type="submit" value="Login with Github" />
       </form>
     </>
--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Dashboard, {loader as dashboardLoader} from "./routes/dashboard";
